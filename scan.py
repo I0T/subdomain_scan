4acbc344ecb8727588aa4145fea58a48467eb409
I0T <1121887411@qq.com>
2017-02-14 16:16:10 +0800

    Create scan.py

---
+++ scan.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*-coding: utf-8-*-
+# @Time    : 17/2/14
+# @Author  : IOT
+# @File    : scan.py
+
+import socket
+
+import gevent
+from gevent.pool import Pool
+from gevent import monkey
+monkey.patch_all()
+
+def get_ip(url):
+    try:
+        ips_socket = socket.getaddrinfo(url,0,2,0,6,0)
+        if len(ips_socket) != 1:
+            ip = 'not_real %s'% ips_socket[-1][-1][0]
+        else:
+            ip = ips_socket[-1][-1][0]
+        url_and_ip = '%s %s' % (url, ip)
+        print(url_and_ip)
+    except:
+        1
+def run(urls):
+    pool = Pool(500)
+    jobs = [pool.spawn(get_ip,url) for url in urls]
+    gevent.wait(jobs)
+if __name__ == "__main__":
+    domain = 'google.com'
+    urls_0 = ['%s'%domain.split('.')[0]]
+    urls = ['%s.%s'%(url_0.strip('\n'),domain) for url_0 in open('dic.txt').readlines()]
+    run(urls)
